2e47e2c8896c9bf439e16f9b49519876c6c14b5f
Jan Almeroth <jan@almeroth.com>
2017-09-04 15:03:10 +0200

    Add docstrings for MediaStatus

--- pymusiccast/__init__.py
+++ pymusiccast/__init__.py
@@ -74,29 +74,36 @@ class MediaStatus(object):
 
     @property
     def media_duration(self):
+        """Duration of current playing media in seconds."""
         return self.total_time
 
     @property
     def media_image_url(self):
+        """Image url of current playing media."""
         return "http://{}{}".format(self.host, self.albumart_url)
 
     @property
     def media_artist(self):
+        """Artist of current playing media, music track only."""
         return self.artist
 
     @property
     def media_album(self):
+        """Album of current playing media, music track only."""
         return self.album
 
     @property
     def media_track(self):
+        """Track number of current playing media, music track only."""
         return self.track
 
     @property
     def media_title(self):
+        """Title of current playing media."""
         return self.media_track
 
     def initialize(self, data):
+        """ initialize variable from loaded data """
         for item in data:
             if hasattr(self, item):
                 setattr(self, item, data[item])
